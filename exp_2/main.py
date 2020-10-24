1ee047cc153943af15b439453233ee902562a0a9
Aaron <70065998+jadeduo@users.noreply.github.com>
2020-10-24 15:51:36 +0800

    Add files via upload

--- exp_2/main.py
+++ exp_2/main.py
@@ -80,10 +80,26 @@ if __name__ == '__main__':
     parser.add_argument('-train_file', type=str, default='./data/train.csv')
     parser.add_argument('-dev_file', type=str, default='./data/dev.csv')
     parser.add_argument('-test_file', type=str, default='./data/test.csv')
-    parser.add_argument('-save_path', type=str, default='./model.pkl')
-    parser.add_argument('-model', type=str, default="cnn", help="[cnn, bilstm]")
+    parser.add_argument('-save_path', type=str, default='./model2.pkl')
+    parser.add_argument('-model', type=str, default="bilstm", help="[cnn, bilstm]")
 
-    parser.add_argument('-batch_size', type=int, default=16)
+    #调参
+    # model=['cnn','bilstm']
+    # batch_size_list = [8, 16, 32, 64]
+    # embedding_size_list=[64,128,256]
+    # hidden_size_list=[64,128,256]
+    # learning_rate_list=[1e-2, 5e-3, 1e-3, 5e-4, 1e-4]
+    # dropout_list= [0.1, 0.2, 0.3, 0.4, 0.5]
+
+
+    # #对二维列表初始化
+    # batch_cnn_train_1 = [[0] * 20 for _ in range(4)]
+    # batch_cnn_dev_1 = [[0] * 20 for _ in range(4)]
+
+    batch_cnn_train = [0]*20
+    batch_cnn_dev = [0]*20
+
+    parser.add_argument('-batch_size', type=int, default=64)
     parser.add_argument('-embedding_size', type=int, default=128)
     parser.add_argument('-hidden_size', type=int, default=128)
     parser.add_argument('-learning_rate', type=float, default=1e-3)
@@ -123,6 +139,7 @@ if __name__ == '__main__':
         model = BiLSTM.Model(vocab_size=len(vocab),
                              embedding_size=args.embedding_size,
                              hidden_size=args.hidden_size,
+                             num_layers=1,
                              dropout=args.dropout)
 
     if torch.cuda.is_available():
@@ -134,14 +151,31 @@ if __name__ == '__main__':
     for i in range(args.epochs):
         print("Epoch: {} ################################".format(i))
         train_loss, train_acc = trainer.train(train_loader)
+
         dev_loss, dev_acc = trainer.evaluate(dev_loader)
+
+        #训练集和验证集的 loss
+        batch_cnn_train[i]=round(train_loss,4)
+        batch_cnn_dev[i]=round(dev_loss,4)
+
+
         print("Train Loss: {:.4f} Acc: {:.4f}".format(train_loss, train_acc))
         print("Dev   Loss: {:.4f} Acc: {:.4f}".format(dev_loss, dev_acc))
         if dev_acc > best_acc:
             best_acc = dev_acc
             trainer.save(args.save_path)
         print("###########################################")
-    trainer.load(args.save_path)
-    test_loss, test_acc = trainer.evaluate(test_loader)
-    print("Test   Loss: {:.4f} Acc: {:.4f}".format(test_loss, test_acc))
+
+        trainer.load(args.save_path)
+        test_loss, test_acc = trainer.evaluate(test_loader)
+        print("Test   Loss: {:.4f} Acc: {:.4f}".format(test_loss, test_acc))
+
+    print(batch_cnn_train)
+    print(batch_cnn_dev)
+
+
+
+    #test_loss,test_acc
+    #调整 batch size 参数，取值分别为 [8, 16, 32, 64] ，画出 TextCNN 和 BiLSTM 训练集和验证集的 loss 折线图。
+
 
